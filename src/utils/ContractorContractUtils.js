a9b12aaf8b426ca37ae753e65b48a1bd9e285e3d
OmkarAnbhule <omkaranbhule75@gmail.com>
2024-11-18 20:54:10 +0530

    update cache

--- src/utils/ContractorContractUtils.js
+++ src/utils/ContractorContractUtils.js
@@ -2,6 +2,7 @@ export const fetchContactCards = (setDisplayCards, setLoading) => {
     try {
         fetch('/api/v1/contractorContact', {
             method: 'GET',
+            cache: 'default'
         })
             .then((res) => res.json())
             .then(({ data }) => {
